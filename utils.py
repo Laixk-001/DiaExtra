e7e338387087fcfed262bc99e806db45f522ae33
赖笑慷 <laixiaokang@4paradigm.com>
2024-10-24 19:34:31 +0800

    up

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@
 import torch
 import random
 import numpy as np
+import torch.distributed
 from transformers import set_seed
 import json
 import os
@@ -110,3 +111,125 @@ class QwenPromptDataSet(Dataset):
         assert len(input_ids) <= self.max_len
 
         return input_ids, labels, skip_flag
+
+    def __len__(self):
+        return len(self.all_data)
+
+    def __getitem__(self,item):
+        instance = self.all_data[item]
+        return instance
+
+class DataCollator(object):
+    """DataLoader所需DataCollator类"""
+    def __init__(self, tokenizer):
+        """
+        初始化函数
+        Args:
+            tokenizer: 分词器
+        """
+        self.tokenizer = tokenizer
+        # padding标记
+        self.pad_token_id = tokenizer.pad_token_id
+    
+    def __call__(self, batch):
+        """
+        回调函数
+        Args:
+            batch: 输入数据
+        
+        """
+        # 获取一个Batch内的最大长度
+        lengths = [len(instance["input_ids"]) for instance in batch]
+        batch_max_len = math.ceil(max(lengths) / 8)* 8
+        # 遍历Batch内容
+        input_ids_batch, labels_batch = [], []
+        for instance in batch:
+            input_ids = instance["input_ids"]
+            labels = instance["labels"]
+            # 将Batch内的数据填充到最大长度
+            padding_len = batch_max_len - len(input_ids)
+            input_ids = input_ids + [self.pad_token_id] * padding_len
+            labels = labels + [-100] * padding_len
+
+            input_ids_batch.append(input_ids)
+            labels_batch.append(labels)
+        #将结果转化为torch.tensor并返回
+        return {"input_ids":torch.tensor(input_ids_batch,dtype=torch.long),"labels":torch.tensor(labels_batch,dtype=torch.long)}
+
+def print_trainable_parameters(model):
+    """打印可训练的参数"""
+    trainable_parameters = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        num_params = param.numel()
+        if num_params == 0 and hasattr(param, "ds_numel"):
+            num_params = param.ds_numel
+        
+        all_param += num_params
+        if param.requires_grad:
+            trainable_parameters += num_params
+    print("trainable params:{} || all params: {} || trainable%: {}".format(trainable_parameters, all_param, 100 * trainable_parameters / all_param))
+
+def print_rank_0(msg, rank=0):
+    """多卡训练时，打印rank=0上的信息"""
+    if rank <= 0:
+        return (msg)
+
+def to_device(batch, device):
+    """将Batch内的数据内容，设置到对应的device上"""
+    output = {}
+    for k,v in batch.items():
+        try:
+            output[k] = v.to(device)
+        except:
+            output[k] = v
+    return output
+
+def set_random_seed(seed):
+    if seed is not None:
+        set_seed(seed)
+        random.seed(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+
+def find_all_linear_names(model):
+    """找到模型中所有的线性层"""
+    cls = bnb.nn.Linear4bit
+    lora_module_names = set()
+    for name, module in model.named_modules():
+        if isinstance(module,cls):
+            names = name.split('.')
+            lora_module_names.add(names[0] if len(names) == 1 else names[-1])
+    if 'lm_head' in lora_module_names:
+        lora_module_names.remove('lm_head')
+    return list(lora_module_names)
+
+def evaluation(model, eval_dataloader, device):
+    """模型验证，计算验证集的PPL值"""
+    model.eval()
+    total_loss = 0
+    for step, batch in tqdm(enumerate(eval_dataloader), total=len(eval_dataloader),unit="batch"):
+        batch = to_device(batch, device)
+        with torch.no_grad():
+            outputs = model(**batch, use_cache=False)
+            loss=outputs.loss
+            total_loss += loss.float()
+    total_loss = total_loss /(step + 1)
+
+    try:
+        perplexity = torch.exp(total_loss)
+    except OverflowError:
+        perplexity = float("inf")
+    
+    try:
+        perplexity = get_all_reduce_mean(perplexity).item()
+    except:
+        pass
+    model.train()
+    return perplexity
+
+def get_all_reduce_mean(tensor):
+    torch.distributed.all_reduce(tensor, op=torch.distributed.ReduceOp.SUM)
+    tensor = tensor / torch.distributed.get_world_size()
+    return tensor
